b5cc919eba93f5bf67ef22134b9889bfb3f9764d
coderLgx <2190685582@qq.com>
2023-07-08 22:37:32 +0800

    Initial commit

---
+++ consumer/src/main/java/coderLgx/Consumer.java
@@ -0,0 +1,15 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Component
+public class Consumer {
+    @
+    @RequestMapping("/consumer")
+    @ResponseBody
+    public String c() {
+        return
+    }
+}
---
+++ provider/src/main/java/coderLgx/Provider.java
@@ -0,0 +1,2 @@
+package coderLgx;public class Provider {
+}
